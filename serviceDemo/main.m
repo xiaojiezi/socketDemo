43522ba0d17ba701ff51b81994759e18dc6e6d5d
yangjie <1575211330@qq.com>
2016-03-20 16:20:53 +0800

    Initial Commit

---
+++ serviceDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  serviceDemo
+//
+//  Created by yangjie on 16-3-20.
+//  Copyright (c) 2016年 YJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
